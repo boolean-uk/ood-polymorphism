8c6a5051c39308f3f90a1883539eaffba37fd521
MiloszSobczyk <m.sobczyk.official@gmail.com>
2023-07-24 09:56:26 +0200

    Added polymorhpism

--- src/Include.js
+++ src/Include.js
@@ -4,10 +4,9 @@ class Include {
     this.val2 = val2
   }
 
-  checkInclude() {
-    if (this.val1 instanceof String || this.val1 instanceof Array) {
+  check() {
+    if (this.val1 instanceof String || this.val1 instanceof Array)
       return this.val1.includes(this.val2)
-    }
     return false
   }
 }
