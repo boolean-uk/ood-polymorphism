7a1e9736a63fc05c8ebdaba29dadfe9cb36a7eeb
s21009 <s21009@pjwstk.edu.pl>
2023-07-24 10:38:57 +0200

    fixed names of variables

--- src/Equal.js
+++ src/Equal.js
@@ -2,7 +2,7 @@ const Assertion = require("./Assertion");
 
 class Equal extends Assertion {
   constructor(val1, val2) {
-    super(val1, val2); // Call super() to initialize the parent class
+    super(val1, val2); 
   }
 }
 
