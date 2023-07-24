c886f1491329389e9204a03171f1d679e18f1871
Damian Gurdak <damian.gurdak@talan.com>
2023-07-24 11:14:23 +0300

    create new class object

--- src/Include.js
+++ src/Include.js
@@ -1,10 +1,10 @@
-class Include {
+class Include extends Object {
   constructor(val1, val2) {
     this.val1 = val1
     this.val2 = val2
   }
 
-  checkInclude() {
+  checkeEqual() {
     if (this.val1 instanceof String || this.val1 instanceof Array) {
       return this.val1.includes(this.val2)
     }
