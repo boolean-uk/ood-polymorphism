fdb3f8f4b8cbddacb5ac1ceb8adefec5923df9aa
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-24 10:08:32 +0200

    Refactor comparisons

--- src/Equal.js
+++ src/Equal.js
@@ -1,10 +1,11 @@
-class Equal {
-  constructor(val1, val2) {
-    this.val1 = val1
-    this.val2 = val2
+const Comparison = require("./Comparison");
+
+class Equal extends Comparison {
+  checkEqual() {
+    return this.val1 === this.val2
   }
 
-  checkEqual() {
+  compare() {
     return this.val1 === this.val2
   }
 }
